97166734d6630a63414874c6bedd1f91e317492b
Gomeskk <gomesj778@gmail.com>
2024-05-13 13:00:53 +0100

    testing printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:39:50 by joafaust          #+#    #+#             */
-/*   Updated: 2024/05/07 16:39:50 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/05/13 12:42:47 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,3 +109,57 @@ int	ft_putpointer(unsigned long int n, int flag, char	*basestr)
 	return (retn);
 }
 
+/* int	main(void)
+{
+	ft_printf("%d \n", ft_printf("ola%%%"));
+	//printf("%d \n", printf("ola%%%"));
+ 
+	int teste = printf("hello world\n");
+	printf("imprimiu: %d\n", teste);
+	int teste2 = ft_printf("hello world\n");
+	ft_printf("imprimiu: %d\n", teste2);
+	printf("\n");
+	int x = 42;
+	int x2 = 34;
+	void *ptrx2 = &x2;
+	void *ptr = &x;
+
+	
+	ft_printf("HELLO\n");
+	ft_printf("%d \n", ft_printf(""));
+	ft_printf("%d \n", ft_printf(NULL));
+	ft_printf("CHARACTERS: %c %c\n", 'a', 'b');
+	ft_printf("STRINGS: %s %s\n", "Good", "bye");
+	ft_printf("DECIMAL: %i %d\n", 42, 123);
+	ft_printf ("DECIMALS: %d %d\n", 1977, 650000L);
+	ft_printf("POINTER NULO: %p \n", NULL );
+	ft_printf("POINTER DE x é: %p\n", ptr);
+	ft_printf("POINTER 0: %p %p \n", (void *)0, (void *)0);
+	ft_printf ("DECIMAL e HEXADECIMAL: %d %x %X \n", 255, 255, 255);
+	ft_printf ("HEXADECIMAL 0: %x %X\n", 0, 0);
+	ft_printf ("Unsigned int: %u %u\n",  2147483647, UINT_MAX);
+	ft_printf ("Unsigned int: %u \n", UINT_MAX);
+	ft_printf ("Mix: %c, %s, %p, %d, %i, %x, %X, %u, %%, all mixed\n", 'A',
+		"Mix", ptrx2, 34, 34, 255, 255, UINT_MAX);
+
+
+	ft_printf("\n");
+	
+	printf("Hello\n");
+	printf("%d \n", printf(""));
+	printf("%d \n", printf(NULL));
+	printf("Characters: %c %c\n", 'a', 'b');
+	printf("Strings: %s %s\n", "Good", "bye");
+	printf("Decimal: %d %d\n", 42, 123);
+	printf ("Decimals: %d %ld\n", 1977, 650000L);
+	printf("Pointer nulo: %p \n", NULL );
+	printf("Pointer de x é: %p\n", ptr);
+	printf("Pointer 0: %p %p \n", (void *)0, (void *)0);
+	printf ("Decimal e hexadecimal: %d %x %X \n", 255, 255, 255);
+	printf (" Hexadecimal 0: %x %X\n", 0, 0);
+	printf ("Unsigned int: %u %u\n", 2147483647, UINT_MAX);
+	printf ("Unsigned int: %u \n", UINT_MAX);
+	printf ("Mix: %c, %s, %p, %d, %i, %x, %X, %u, %%, all mixed\n", 'A', "Mix",
+		ptrx2, 34, 34, 255, 255, UINT_MAX);
+	return (0);
+} */
